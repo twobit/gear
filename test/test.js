1f7c80fcf097e5d9d2210242a043f05e513a4025
Stephen Murphy <stephen@hypernaut.com>
2012-05-09 22:48:50 -0700

    added log task and simplified code

--- test/test.js
+++ test/test.js
@@ -5,21 +5,5 @@ var task = new TaskJS()
     .files(['index.js', 'package.json', 'foo.txt', 'bar'])
     .concat()
     .inspect()
+    .log('WTF!!!')
     .run();
-
-// Buildy Style
-var task2 = new TaskJS()
-    .task('files', ['index.js', 'package.json', 'foo.txt', 'bar'])
-    .task('concat')
-    .task('inspect')
-    .run();
-
-// TaskJS Advanced
-var task3 = new TaskJS().flow({
-    read_files:      {task: 'files', options: [['index.js', 'test/test.js']]},
-    concat_files:    {requires: ['read_files'], task: 'concat'},
-    inspect_results: {requires: ['concat_files'], task: 'inspect'},
-    read_files2:      {task: 'files', options: [['package.js']]},
-    inspect_results2: {requires: ['read_files2'], task: 'inspect'},
-    inspect_1_and_2: {requires: ['inspect_results2', 'inspect_results'], task: 'inspect'}
-});
