9656b94aeb745811b15abf370ccef93d2858bdaa
Stephen Murphy <stephen@hypernaut.com>
2012-05-10 01:13:22 -0700

    split flow and queue classes

--- test/flow.js
+++ test/flow.js
@@ -1,10 +1,12 @@
 var TaskJS = require('../lib/task');
 
-var task = new TaskJS().flow({
+new TaskJS.Flow().flow({
     read_files:      {task: 'files', options: [['index.js', 'test/test.js']]},
     concat_files:    {requires: ['read_files'], task: 'concat'},
     inspect_results: {requires: ['concat_files'], task: 'inspect'},
     read_files2:      {task: 'files', options: [['package.js']]},
     inspect_results2: {requires: ['read_files2'], task: 'inspect'},
     inspect_1_and_2: {requires: ['inspect_results2', 'inspect_results'], task: 'inspect'}
+}, function(err, results) {
+    console.log(results);
 });
---
+++ test/queue.js
@@ -0,0 +1,8 @@
+var TaskJS = require('../lib/task');
+
+var queue = new TaskJS.Queue()
+    .files(['index.js', 'package.json', 'foo.txt', 'bar'])
+    .concat()
+    .inspect()
+    .log('WTF!!!')
+    .run();
