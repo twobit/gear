19dbbd4e2a34e24a42d67b815866338b883d7796
Stephen Murphy <stephen@hypernaut.com>
2012-05-09 18:58:15 -0700

    improved task input/output handling

--- test/test.js
+++ test/test.js
@@ -1,5 +1,6 @@
 var TaskJS = require('../lib/task');
 
+/*
 // TaskJS Style
 var task = new TaskJS()
     .files(['index.js', 'package.json', 'foo.txt', 'bar'])
@@ -13,10 +14,11 @@ var task2 = new TaskJS()
     .task('concat')
     .task('inspect')
     .run();
-/*
+*/
+
 // TaskJS Advanced
 var task3 = new TaskJS().flow({
-    read_files: {task: 'files', options: ['index.js']},
+    read_files: {task: 'files', options: [['index.js']]},
     concat_files: {task: 'concat', requires: ['read_files']},
     inspect_results: {task: 'inspect', requires: ['concat_files']}
-});*/
+});
