a418ec84bdde73eb6765328d066caa2924edd86a
Stephen Murphy <stephen@hypernaut.com>
2012-05-10 00:04:46 -0700

    created registry

--- test/test.js
+++ test/test.js
@@ -1,7 +1,13 @@
 var TaskJS = require('../lib/task');
 
+var registry = new TaskJS.Registry();
+registry.loadFile('../lib/tasks/files');
+registry.loadFile('../lib/tasks/concat');
+registry.loadFile('../lib/tasks/log');
+registry.loadFile('../lib/tasks/inspect');
+
 // TaskJS Style
-var task = new TaskJS()
+var task = new TaskJS.TaskJS(registry)
     .files(['index.js', 'package.json', 'foo.txt', 'bar'])
     .concat()
     .inspect()
