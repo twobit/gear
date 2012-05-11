ecaea66d4942c5413d41b89a6e03df2b6f31fb60
Stephen Murphy <stephen@hypernaut.com>
2012-05-10 18:47:17 -0700

    added new test

---
+++ test/tasks/test_files.js
@@ -0,0 +1,11 @@
+var should = require('should');
+
+describe('Queue', function() {
+    describe('.task', function() {
+        it('should equal true', function() {
+            var user = {};
+
+            user.should.have.property('foo');
+        });
+    });
+});
