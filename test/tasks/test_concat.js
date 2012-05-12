9b285ed6e2407d8cb335ae00f3256e690926d6f4
Stephen Murphy <stephen@hypernaut.com>
2012-05-11 17:06:29 -0700

    updated commit task and test

--- test/tasks/test_concat.js
+++ test/tasks/test_concat.js
@@ -2,7 +2,7 @@ var should = require('should'),
     concat = require('../../lib/tasks/concat').concat,
     fixtures = {
         objects: [{meta: {}, content: 'abc'}, {meta: {}, content: 'def'}],
-        object: []
+        object: [{meta: {}, content: 'abc'}]
     };
 
 describe('concat()', function() {
@@ -10,6 +10,21 @@ describe('concat()', function() {
         concat(null, fixtures.objects, console, function(err, objects) {
             objects.should.have.lengthOf(1);
             objects[0].content.should.equal('abcdef');
+            done(err);
+        });
+    });
+
+    it('should not concat one object', function(done) {
+        concat(null, fixtures.object, console, function(err, objects) {
+            objects.should.have.lengthOf(1);
+            objects[0].content.should.equal('abc');
+            done(err);
+        });
+    });
+
+    it('should handle empty objects', function(done) {
+        concat(null, [], console, function(err, objects) {
+            objects.should.have.lengthOf(0);
             done(err);
         });
     });
