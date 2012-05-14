80b54876e942c9ac4a6a97e47b5cc92d54cb34cb
Stephen Murphy <stephen@hypernaut.com>
2012-05-14 12:55:41 -0700

    fixed a few bugs, updated test

--- test/flow.js
+++ test/flow.js
@@ -1,8 +1,22 @@
-var flow = require('../lib/tasks/flow').flow,
+var taskjs = require('../lib/task'),
+    flow = require('../lib/tasks/flow').flow,
     fixtures = {
         files: ['test/fixtures/test1.js'],
         parallel_files: ['test/fixtures/test2.js']
     };
+
+describe('taskjs.task()', function() {
+    it('should wrap task correctly', function() {
+        taskjs.task('flow', {params: {
+            read_files:      {task: 'files', params: fixtures.files},
+            concat_files:    {requires: ['read_files'], task: 'concat'},
+            inspect_results: {requires: ['concat_files'], task: 'inspect'},
+            read_files2:      {task: 'files', params: fixtures.parallel_files},
+            inspect_results2: {requires: ['read_files2'], task: 'inspect'},
+            inspect_1_and_2: {requires: ['inspect_results2', 'inspect_results'], task: 'log', params: 'Done'}
+        }});
+    });
+});
 
 describe('flow()', function() {
     it('should execute complex flows', function(done) {
