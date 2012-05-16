349b72e9ee5252a2a2543bcc4ce0181cafbb1417
Stephen Murphy <stephen@hypernaut.com>
2012-05-16 16:59:16 -0700

    updated index exports

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
-module.exports = require('./lib/task');
+var tasks = [
+    './lib/concat',
+    './lib/files',
+    //'./lib/fork',
+    './lib/inspect',
+    './lib/log',
+    './lib/write'
+];
+
+tasks.forEach(function(task) {
+    var mod = require(task),
+        name;
+
+    for (name in mod) {
+        exports[name] = mod[name];
+    }
+});
