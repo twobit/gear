d3044e8b766d81175b8fd9fd4c7be56f3f10e452
Stephen Murphy <stephen@hypernaut.com>
2012-05-17 01:41:53 -0700

    added new test

--- test/tasks.js
+++ test/tasks.js
@@ -1,13 +1,17 @@
-var core = require('taskjs-core'),
+var should = require('should'),
+    core = require('taskjs-core'),
     path = require('path'),
     fixtures = {
-        files: [{file: 'test/fixtures/test1.js'}, {file: 'test/fixtures/test2.js'}]
+        files: [{file: 'test/fixtures/test1.js'}, {file: 'test/fixtures/test2.js'}],
+        missing_files: [{file: 'test/fixtures/missing_file.js'}]
     };
 
 describe('Queue', function() {
     describe('run()', function() {
+        var options = {registry: core.createRegistry({filename: path.join(process.cwd(), './index.js')})};
+
         it('should execute chained tasks', function(done) {
-            core.createQueue({registry: core.createRegistry({filename: path.join(process.cwd(), './index.js')})})
+            core.createQueue(options)
                 .load(fixtures.files)
                 .concat()
                 .inspect()
@@ -17,8 +21,19 @@ describe('Queue', function() {
                 });
         });
 
+        it('should handle errors', function(done) {
+            core.createQueue(options)
+                .load(fixtures.missing_files)
+                .concat()
+                .inspect()
+                .run(function(err, results) {
+                    should.exist(err);
+                    done();
+                });
+        });
+
         it('should handle forks', function(done) {
-            core.createQueue({registry: core.createRegistry({filename: path.join(process.cwd(), './index.js')})})
+            core.createQueue(options)
                 .fork({
                     load: {task: 'load', options: fixtures.files},
                     log: {task: 'log', options: 'Finished', requires: ['load']}
