fdfd19dedf117eff6031f2fb040a0f617d04fe66
Stephen Murphy <stephen@hypernaut.com>
2012-05-17 01:38:54 -0700

    added new test

--- test/fork.js
+++ test/fork.js
@@ -1,8 +1,10 @@
-var path = require('path'),
+var should = require('should'),
+    path = require('path'),
     core = require('taskjs-core'),
     fork = require('../lib/fork').fork.fn,
     fixtures = {
         files: [{file: 'test/fixtures/test1.js'}],
+        missing_files: [{file: 'test/fixtures/missing_file.js'}],
         parallel_files: [{file: 'test/fixtures/test2.js'}]
     };
 
@@ -21,6 +23,19 @@ describe('taskjs.queue()', function() {
 });
 
 describe('fork()', function() {
+    it('should handle err', function(done) {
+        var registry = {
+            _registry: core.createRegistry({filename: path.join(process.cwd(), './index.js')})
+        };
+
+        fork.call(registry, {
+            read_files:      {task: 'load', options: fixtures.missing_files}
+        }, [], console, function(err, results) {
+            should.exist(err);
+            done();
+        });
+    });
+
     it('should execute complex tasks', function(done) {
         var registry = {
             _registry: core.createRegistry({filename: path.join(process.cwd(), './index.js')})
@@ -31,8 +46,8 @@ describe('fork()', function() {
             concat_files:    {requires: ['read_files'], task: 'concat'},
             inspect_results: {requires: ['concat_files'], task: 'inspect'},
             read_files2:      {task: 'load', options: fixtures.parallel_files},
-            inspect_results2: {requires: ['read_files2'], task: 'inspect'}
-            //inspect_1_and_2: {requires: ['inspect_results', 'inspect_results2'], task: 'log', options: 'Done'}
+            inspect_results2: {requires: ['read_files2'], task: 'inspect'},
+            inspect_1_and_2: {requires: ['inspect_results', 'inspect_results2'], task: 'log', options: 'Done'}
         }, [], console, function(err, results) {
             done(err);
         });
