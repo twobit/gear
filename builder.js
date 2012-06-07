51f99c1bb8de4f27573feb7d881c495b48953801
Stephen Murphy <stephen@hypernaut.com>
2012-06-07 01:27:36 -0700

    bumped version

--- builder.js
+++ builder.js
@@ -1,4 +1,10 @@
 /*
+ * Copyright (c) 2011-2012, Yahoo! Inc.  All rights reserved.
+ * Copyrights licensed under the New BSD License.
+ * See the accompanying LICENSE file for terms.
+ */
+
+/*
  * Bootstraps browser based Gear.js
  */
 var path = require('path'),
