aa81cc01a3137200085050b217e540a4a1abda2e
Stephen Murphy <stephen@hypernaut.com>
2012-06-13 12:09:00 -0700

    separating docs from code

--- test/registry.js
+++
@@ -1,9 +0,0 @@
-var Registry = require('../lib/registry').Registry;
-
-describe('Registry', function() {
-    describe('load()', function() {
-        it('should load modules');
-        it('should load directories');
-        it('should load files');
-    });
-});
