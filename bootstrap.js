f254fd3737b345e0eb726acfb555e11eeb8e951a
Stephen Murphy <stephen@hypernaut.com>
2012-06-15 15:19:08 -0700

    cleaned up bootstrapper

--- bootstrap.js
+++ bootstrap.js
@@ -9,79 +9,61 @@
  */
 var path = require('path'),
     gear = require(path.join(process.cwd(), 'index')),
-    handlebars = require('./node_modules/gear-lib/node_modules/handlebars');
+    handlebars = require('./node_modules/gear-lib/node_modules/handlebars'),
+    wrap = handlebars.compile(
+        "\n\ndefine('{{name}}', ['require', 'exports'{{#modules}}, '{{.}}'{{/modules}}], function(require, exports{{#input}}, {{.}}{{/input}}) {\n\n" +
+        "{{{result}}}\n\n" +
+        "});\n\n"
+    ),
+    helper = handlebars.compile(
+        "define('../blob', ['require', 'exports', 'blob'], function(require, exports, blob) {\n" +
+        "exports.Blob = blob.Blob;\n" +
+        "});\n\n" +
+        "define('./default_tasks', ['require', 'exports'{{#tasks}}, '{{.}}'{{/tasks}}], function(require, exports) {\n" +
+        "var tasks = [];\n" +
+        "{{#tasks}}tasks.push(require('{{.}}'));{{/tasks}}\n" +
+        "tasks.forEach(function(mod) {for (var task in mod) {exports[task] = mod[task];}});\n" +
+        "});\n\n" +
+        "define('gear', ['require', 'exports', 'blob', 'registry', 'queue'], function(require, exports, blob, registry, queue) {\n" +
+        "exports.Blob = blob.Blob; exports.Registry = registry.Registry; exports.Queue = queue.Queue;\n" +
+        "});\n\n"
+    ),
+    files = {
+        'vendor/require.js': {},
+        'node_modules/async/lib/async.js': {name: 'async', modules: ['module'], input: ['module']},
+        'lib/blob.js': {name: './blob'},
+        'lib/tasks/concat.js': {name: './tasks/concat', task: true},
+        'lib/tasks/core.js': {name: './tasks/core', modules: ['../blob'], task: true},
+        'lib/tasks/read.js': {name: './tasks/read', modules: ['../blob'], task: true},
+        'lib/tasks/write.js': {name: './tasks/write', task: true},
+        'lib/tasks/tasks.js': {name: './tasks/tasks', modules: ['async'], task: true},
+        'lib/registry.js': {name: './registry', modules: ['./tasks/concat', './default_tasks']},
+        'lib/queue.js': {name: './queue', modules: ['async', './registry', './blob']}
+    },
+    tasks = [];
 
-var wrap = handlebars.compile(
-    "\n\ndefine('{{name}}', ['require', 'exports'{{#modules}}, '{{.}}'{{/modules}}], function(require, exports{{#input}}, {{.}}{{/input}}) {\n\n" +
-    "{{{result}}}\n\n" +
-    "});\n\n"
-);
-
-var helper = handlebars.compile(
-    "define('../blob', ['require', 'exports', 'blob'], function(require, exports, blob) {\n" +
-    "exports.Blob = blob.Blob;\n" +
-    "});\n\n" +
-    "define('./default_tasks', ['require', 'exports'{{#tasks}}, '{{.}}'{{/tasks}}], function(require, exports) {\n" +
-    "var tasks = [];\n" +
-    "{{#tasks}}tasks.push(require('{{.}}'));{{/tasks}}\n" +
-    "tasks.forEach(function(mod) {for (var task in mod) {exports[task] = mod[task];}});\n" +
-    "});\n\n" +
-    "define('gear', ['require', 'exports', 'blob', 'registry', 'queue'], function(require, exports, blob, registry, queue) {\n" +
-    "exports.Blob = blob.Blob; exports.Registry = registry.Registry; exports.Queue = queue.Queue;\n" +
-    "});\n\n"
-);
-
-var files = {
-    'vendor/require.js': {},
-    'node_modules/async/lib/async.js': {name: 'async', modules: ['module'], input: ['module']},
-    'lib/blob.js': {name: './blob'},
-    'lib/tasks/concat.js': {name: './tasks/concat', task: true},
-    'lib/tasks/core.js': {name: './tasks/core', modules: ['../blob'], task: true},
-    'lib/tasks/read.js': {name: './tasks/read', modules: ['../blob'], task: true},
-    'lib/tasks/write.js': {name: './tasks/write', task: true},
-    'lib/tasks/tasks.js': {name: './tasks/tasks', modules: ['async'], task: true},
-    'lib/registry.js': {name: './registry', modules: ['./tasks/concat', './default_tasks']},
-    'lib/queue.js': {name: './queue', modules: ['async', './registry', './blob']}
-};
-
-var tasks = [];
-for (var task in files) {
-    if (files[task].task) {
-        tasks.push(files[task].name);
-    }
-}
+for (var key in files) {if (files[key].task) {tasks.push(files[key].name);}}
 
 new gear.Queue({registry: new gear.Registry({module: 'gear-lib'})})
     .read(Object.keys(files))
+    .load(helper({tasks: tasks}))
     .jslint({nomen: true, sloppy: true, white: true, vars: true, callback: function(blob) {
         //console.log(blob.name ? blob.name : 'inline', blob.jslint);
     }})
     .concat({callback: function(blob) {
-        var obj = files[blob.name];
-        if (obj.name) {
+        if (blob.name in files && files[blob.name].name) {
+            var obj = files[blob.name];
             var vars = {result: blob.result};
             Object.keys(obj).forEach(function(attr) {vars[attr] = obj[attr];});
             return wrap(vars);
         }
         return blob.result;
     }})
-    .load(helper({tasks: tasks}))
-    .concat()
     .tasks({
         dev:      {task: ['write', 'build/gear.js']},
-
         prodmin:  {task: 'jsminify'},
         prod:     {requires: 'prodmin', task: ['write', 'build/gear.min.js']},
-        
         join:     {requires: ['dev', 'prod']}
-        /*
-        fullraw: {task: ['read', 'node_modules/gear-lib/build/gear-lib.js']},
-        full: {requires: 'fullraw', task: 'concat'},
-        devfull: {requires: 'full', task: ['write', 'build/gear-full.js']},
-        prodfullmin: {requires: 'full', task: 'jsminify'},
-        prodfull: {requires: 'prodfullmin', task: ['write', 'build/gear-full.min.js']},
-
-        */
     })
     .run(function(err, results) {
         if (err) {
