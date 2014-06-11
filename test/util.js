4c50742420aab7c4ffc2b7bd6527676ad95c879e
Stephen Murphy <smurphy3@apple.com>
2014-06-10 22:53:20 -0700

    s/file/Util/g

--- test/util.js
+++ test/util.js
@@ -1,32 +1,30 @@
 var lib = process.env.GEAR_COVER ? '../lib-cov/' : '../lib/',
-    file = require(lib + 'file').file,
+    Util = require(lib + 'util'),
     should = require('should');
 
-describe('file', function() {
-
+describe('Util', function() {
     describe('readJSON', function() {
         it('should read JSON', function() {
-            var res = file.readJSON('./test/fixtures/test.json');
+            var res = Util.readJSON('./test/fixtures/test.json');
             res.x.should.equal(12);
         });
 
         it('should not read JSONC', function() {
             should(function() {
-                file.readJSON('./test/fixtures/test.jsonc');
+                Util.readJSON('./test/fixtures/test.jsonc');
             }).throw();
         });
     });
 
     describe('readJSONC', function() {
         it('should read JSON', function() {
-            var res = file.readJSONC('./test/fixtures/test.json');
+            var res = Util.readJSONC('./test/fixtures/test.json');
             res.x.should.equal(12);
         });
 
         it('should read JSONC', function() {
-            var res = file.readJSONC('./test/fixtures/test.jsonc');
+            var res = Util.readJSONC('./test/fixtures/test.jsonc');
             res.x.should.equal(12);
         });
     });
-
 });
